ed2e16e026f94af510cb6319a11a823ed59a83cb
JOHAN STEVEN CARTAGENA GONZALEZ <j.steven.cartagonzalez@gmail.com>
2022-07-12 15:46:19 -0500

    first commit

---
+++ skater.py
@@ -0,0 +1,54 @@
+class persona():
+    def __init__(self,name):
+        self.nombre = name 
+        
+    def dormir(self):
+        print("estoy durmiendo", self.nombre)
+        
+    def hablar(self,hablar):
+        print("estoy hablando",hablar)
+     
+    def cantar(self):
+        print("estoy cantando",self.nombre)
+        
+        
+bailarina = persona("lorena")
+skater = persona ("andres")
+cantante = persona ("nicole")
+
+bailarina.dormir()
+skater.hablar("hola")        
+cantante.cantar()    
+        
+        
+     
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
